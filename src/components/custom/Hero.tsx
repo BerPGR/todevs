1c3e0207e36e8753c4711fea0394f5b2c5294bd1
Bernardo Matuchewski <bernardopgr01@gmail.com>
2025-06-30 20:37:50 -0300

    feat: hero section

---
+++ src/components/custom/Hero.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import heroImage from "../../../public/undraw_engineering-team_13ax.svg";
+import { Button } from "../ui/button";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center container mx-auto">
+      <div className="flex flex-col gap-10">
+        <h1 className="text-white text-6xl pr-40">Construa projetos incríveis juntos!</h1>
+        <p className="text-white font-light text-2xl pr-20">Crie um projeto, procure colaboradores e expanda seu portfólio</p>
+        <div className="flex items-center gap-10">
+          <Button className="p-6 text-md" variant="default">Criar Projeto</Button>
+          <Button className="p-6 text-md bg-transparent text-white" variant="outline">Explorar Projetos</Button>
+        </div>
+      </div>
+      <img src={heroImage} width={726} className="object-contain"/>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../../components/custom/NavBar'
+import Hero from '../../components/custom/Hero'
 
 const Home = () => {
   return (
     <div className='min-h-screen bg-[#0D1117] p-10'>
       <NavBar />
+      <Hero />
     </div>
   )
 }
