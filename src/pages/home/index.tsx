de24b190f171325c82a6ac58f90efaf2336e4d63
Bernardo Matuchewski <bernardopgr01@gmail.com>
2025-07-01 19:26:27 -0300

    feat: added static props for FeedCards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -27,6 +27,7 @@ import { cn } from "../../lib/utils";
 import { Command } from "../../components/ui/command";
 import { Badge } from "../../components/ui/badge";
 import { X } from "lucide-react";
+import CardFeed from "../../components/custom/Feed/CardFeed";
 
 type Category = {
   label: string;
@@ -39,6 +40,41 @@ const CATEGORIES: Category[] = [
   { label: "Produtividade", value: "productivity" },
   { label: "Educação", value: "education" },
   { label: "Bem-estar", value: "wellness" },
+];
+
+const feedCards = [
+  {
+    title: "E-Commerce Platform",
+    creator: "Jane Doe",
+    role: "Front-End Developer",
+    stack: "ReactJS",
+    status: "Open",
+    applications: 3,
+  },
+  {
+    title: "Social Media Dashboard",
+    creator: "John Smith",
+    role: "UI/UX Designer",
+    stack: "Figma",
+    status: "Open",
+    applications: 5,
+  },
+  {
+    title: "Travel Booking App",
+    creator: "Seeeh A.",
+    role: "Flutter Developer",
+    stack: "Flutter",
+    status: "Open",
+    applications: 12,
+  },
+  {
+    title: "Personal Blog Site",
+    creator: "Alox R.",
+    role: "VueJS Devleoper",
+    stack: "VueJS",
+    status: "Open",
+    applications: 7,
+  },
 ];
 
 const Home = () => {
@@ -140,6 +176,15 @@ const Home = () => {
               })}
             </div>
           )}
+        </div>
+      </div>
+
+      <div className="container mx-auto mt-20">
+        <h1 className="text-4xl text-white font-semibold pb-10">Feed</h1>
+        <div className="grid grid-cols-2 grid-rows-2 gap-5">
+          {feedCards.map((card) => (
+            <CardFeed title={card.title} creator={card.creator} role={card.role} stack={card.stack} status={card.status} applications={card.applications}/>
+          ))}
         </div>
       </div>
     </div>
